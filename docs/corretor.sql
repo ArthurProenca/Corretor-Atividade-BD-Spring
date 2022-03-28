61de655bb9eaa2f50a14116811bfb26fb853fa0c
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-03-28 17:32:44 -0300

    Refactore schema corretora

--- docs/corretor.sql
+++ docs/corretor.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE corretor;
-USE corretor;
+CREATE DATABASE corretora;
+USE corretora;
 
 
 CREATE TABLE pessoa
@@ -15,21 +15,22 @@ CREATE TABLE telefone
 (
     telefone   varchar(11),
     cod_pessoa INT NOT NULL,
-    CONSTRAINT fk_cod_pessoa FOREIGN KEY (cod_pessoa) REFERENCES pessoa (cod_pessoa)
+    CONSTRAINT fk_cod_pessoa FOREIGN KEY (cod_pessoa) REFERENCES pessoa (cod_pessoa) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE locador
 (
-    cod_lcd    INT         NOT NULL PRIMARY KEY,
-    e_parceiro VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_cod_lcd FOREIGN KEY (cod_lcd) REFERENCES pessoa (cod_pessoa)
+    cod_lcd    INT NOT NULL PRIMARY KEY,
+    e_parceiro BOOL DEFAULT 0,
+    CONSTRAINT fk_cod_lcd FOREIGN KEY (cod_lcd) REFERENCES pessoa (cod_pessoa) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE corretor
 (
     cod_crt   INT NOT NULL PRIMARY KEY,
-    avaliacao VARCHAR(10),
-    CONSTRAINT fk_cod_crt FOREIGN KEY (cod_crt) REFERENCES pessoa (cod_pessoa)
+    avaliacao INT UNSIGNED DEFAULT 0,
+    CONSTRAINT chk_1_corretor CHECK (avaliacao <= 5),
+    CONSTRAINT fk_cod_crt FOREIGN KEY (cod_crt) REFERENCES pessoa (cod_pessoa) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE corretor_locador
@@ -37,8 +38,9 @@ CREATE TABLE corretor_locador
     cod_lcd  INT   NOT NULL,
     cod_crt  INT   NOT NULL,
     comissao FLOAT NOT NULL,
-    CONSTRAINT fk_cod_lcd_locador FOREIGN KEY (cod_lcd) REFERENCES locador (cod_lcd),
-    CONSTRAINT fk_cod_crt_locador FOREIGN KEY (cod_crt) REFERENCES corretor (cod_crt)
+    CONSTRAINT pk_corretor_locador PRIMARY KEY (cod_lcd, cod_crt),
+    CONSTRAINT fk_cod_lcd_locador FOREIGN KEY (cod_lcd) REFERENCES locador (cod_lcd) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_cod_crt_locador FOREIGN KEY (cod_crt) REFERENCES corretor (cod_crt) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
@@ -46,7 +48,7 @@ CREATE TABLE locatario
 (
     cod_lct   INT  NOT NULL,
     data_nasc DATE NOT NULL,
-    CONSTRAINT fk_cod_lct FOREIGN KEY (cod_lct) REFERENCES pessoa (cod_pessoa)
+    CONSTRAINT fk_cod_lct FOREIGN KEY (cod_lct) REFERENCES pessoa (cod_pessoa) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE associado
@@ -55,7 +57,7 @@ CREATE TABLE associado
     data_nasc       DATE         NOT NULL,
     tipo_associacao VARCHAR(10)  NOT NULL,
     cod_lcd         INT          NOT NULL,
-    CONSTRAINT fk_cod_lcd_associado FOREIGN KEY (cod_lcd) REFERENCES locador (cod_lcd)
+    CONSTRAINT fk_cod_lcd_associado FOREIGN KEY (cod_lcd) REFERENCES locador (cod_lcd) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE imovel
@@ -73,14 +75,15 @@ CREATE TABLE imovel
     num_apart   INT          NOT NULL,
     tem_garagem BOOL         NOT NULL,
     tipo        VARCHAR(15)  NOT NULL,
-    CONSTRAINT fk_cod_lcd_imovel FOREIGN KEY (cod_lcd) REFERENCES locador (cod_lcd)
+    CONSTRAINT fk_cod_lcd_imovel FOREIGN KEY (cod_lcd) REFERENCES locador (cod_lcd) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE regras_condominio
 (
     cod_imv INT NOT NULL,
     regra   VARCHAR(255),
-    CONSTRAINT fk_cod_imv_condominio FOREIGN KEY (cod_imv) REFERENCES imovel (cod_imv)
+    CONSTRAINT pk_regras_condominio PRIMARY KEY (cod_imv, regra),
+    CONSTRAINT fk_cod_imv_condominio FOREIGN KEY (cod_imv) REFERENCES imovel (cod_imv) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE aluga
@@ -90,10 +93,14 @@ CREATE TABLE aluga
     cod_imv          INT  NOT NULL,
     data_inicio_alug DATE NOT NULL,
     data_fim_alug    DATE NOT NULL,
-    CONSTRAINT fk_cod_lct_aluga FOREIGN KEY (cod_lct) REFERENCES pessoa (cod_pessoa),
-    CONSTRAINT fk_cod_crt_aluga FOREIGN KEY (cod_crt) REFERENCES corretor (cod_crt),
-    CONSTRAINT fk_cod_imv_aluga FOREIGN KEY (cod_imv) REFERENCES imovel (cod_imv)
+    CONSTRAINT pk_aluga PRIMARY KEY (cod_crt, cod_lct, cod_imv),
+    CONSTRAINT fk_cod_lct_aluga FOREIGN KEY (cod_lct) REFERENCES pessoa (cod_pessoa) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_cod_crt_aluga FOREIGN KEY (cod_crt) REFERENCES corretor (cod_crt) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT fk_cod_imv_aluga FOREIGN KEY (cod_imv) REFERENCES imovel (cod_imv) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+
+
 
 
 
