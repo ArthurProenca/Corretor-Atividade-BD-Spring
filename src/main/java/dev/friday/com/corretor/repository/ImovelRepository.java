2e8634e02d1b44923c6d000aa52d6cd9204eabd4
Arthur Rodrigues Proença <arthur.rodriguesproenca@gmail.com>
2022-03-30 19:49:58 -0300

    Refactor

--- src/main/java/dev/friday/com/corretor/repository/ImovelRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.friday.com.corretor.repository;
-
-import dev.friday.com.corretor.entity.Imovel;
-import dev.friday.com.corretor.entity.Locador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImovelRepository extends JpaRepository<Imovel, Locador> {
-}
